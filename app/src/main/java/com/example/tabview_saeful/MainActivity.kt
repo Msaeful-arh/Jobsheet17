f93fb29adbb3d809f91a14c1ac693c6ce58b944a
Msaeful <kiyotakaa787@gmail.com>
2024-05-13 10:45:52 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tabview_saeful/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.tabview_saeful
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val tab = findViewById<TabLayout>(R.id.tablayout)
+        var fragment : Fragment = RedFragment()
+
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.a, fragment)
+        fragmentTransaction.commit()
+
+        tab.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when(tab!!.position) {
+                    0 -> fragment = RedFragment()
+                    1 -> fragment = YellowFragment()
+                    2 -> fragment = BlueFragment()
+                    3 -> fragment = GreenFragment()
+                    4 -> fragment = OrangeFragment()
+
+                }
+                val fT = supportFragmentManager.beginTransaction()
+                fT.replace(R.id.a, fragment)
+                fT.commit()
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+        })
+    }
+}
